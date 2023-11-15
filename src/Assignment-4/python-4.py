1925d2186c595f318fbc7c6ee3323c531be68b1d
kaggullagiri <girikola111@gmail.com>
2023-11-15 07:41:36 +0530

    second commited to Feature_branch
    # Conflicts:
    #	src/Assignment-4/python-4.py

---
+++ src/Assignment-4/python-4.py
@@ -0,0 +1,27 @@
+num1 = int(input("Enter number 1: ")) #user 1
+num2 = int(input("Enter number 2: ")) #user 2
+num3 = int(input("Enter number 3: ")) #user 3
+num4 = int(input("Enter number 4: ")) #user 4
+
+allNum = {num1, num2, num3, num4}
+print("All numbers: ",allNum)
+if(num1 > num2 and num1 > num4) :
+    print("User 1's number  is greater")
+elif(num2 > num3) :
+    print("User 2's number  is greater")
+elif(num3 > num4) :
+    print("User 3's number  is greater")
+elif(num4 > num2) :
+    print("User 4's number  is greater")
+else :
+    print("done")
+
+# first time open the file and make changed something
+
+# code is completed
+
+# second time open the file and make some changes
+
+# code is completed 2nd time
+
+
